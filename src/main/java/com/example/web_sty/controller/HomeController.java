ee93ce9bb5d68411d4acb983f4982e586ad47310
YuYoHan <zxzz8014@naver.com>
2023-04-22 14:57:20 +0900

    [Feat] main 페이지 들어가는거 구현
    REST 방식이 아니기 때문에 처음에 index 페이지를 보여줄 controller가
    필요해서 구현했습니다.

---
+++ src/main/java/com/example/web_sty/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.web_sty.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping({"/", "/home"})
+    public String home() {
+        return "home";
+    }
+
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "/hello";
+    }
+}
